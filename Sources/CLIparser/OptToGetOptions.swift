2fc18deffe0226d2ee4d7611ae7de13ff1ba2b27
Michael Salmon <msalmonse@gmail.com>
2021-06-15 09:50:05 +0200

    Made options public

--- Sources/CLIparser/OptToGetOptions.swift
+++ Sources/CLIparser/OptToGetOptions.swift
@@ -20,31 +20,31 @@ extension OptToGet {
 
         /// option is a flag and has no options
         public static let flag = Options(rawValue: 1 << 0)
-        var isFlag: Bool { contains(.flag) }
+        public var isFlag: Bool { contains(.flag) }
 
         /// option should not be seen in usage
         public static let hidden = Options(rawValue: 1 << 1)
-        var isHidden: Bool { contains(.hidden) }
+        public var isHidden: Bool { contains(.hidden) }
 
         /// arguments that start with - are included
         public static let includeMinus = Options(rawValue: 1 << 2)
-        var minusIncluded: Bool { contains(Self.includeMinus) }
+        public var minusIncluded: Bool { contains(Self.includeMinus) }
 
         /// -- is also included
         public static let includeMinusMinus = Options(rawValue: 1 << 3)
-        var minusMinusIncluded: Bool { contains(Self.includeMinusMinus) }
+        public var minusMinusIncluded: Bool { contains(Self.includeMinusMinus) }
 
         /// option can appear multiple times
         public static let multi = Options(rawValue: 1 << 4)
-        var isMulti: Bool { contains(Self.multi) }
+        public var isMulti: Bool { contains(Self.multi) }
 
         // option is required
         public static let required = Options(rawValue: 1 << 5)
-        var isRequired: Bool { contains(Self.required) }
+        public var isRequired: Bool { contains(Self.required) }
 
         // option cannot be abbreviated
         public static let unabbrev = Options(rawValue: 1 << 6)
-        var canAbbrev: Bool { !contains(Self.unabbrev) }
+        public var canAbbrev: Bool { !contains(Self.unabbrev) }
 
         private static let allNames: [(Options, String)] = [
             (.flag, ".flag"),
